5d46c9e6d4ad7571e303b5c1b2bdb8f3c0b68295
juswaldy <juswaldy@gmail.com>
2022-05-13 00:22:17 +0000

    Task 2

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
 
 app = FastAPI()
+
+
+################################################################################
+# Prepare schemas.
+
+class PredictionRequest(BaseModel):
+    query_string: str
+
+################################################################################
+# Load models.
+
+sentiment_model = pipeline("sentiment-analysis")
+
+################################################################################
+# Setup endpoints.
 
 @app.get("/health")
 def health():
     return "Service is online."
+
+@app.post("/sentiment")
+def sentiment(request: PredictionRequest):
+      # YOUR CODE GOES HERE
+      return sentiment_model(request.query_string)
+
